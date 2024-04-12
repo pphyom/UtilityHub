0a6b138fb89f8877c8ed34c738cb4e55a59d124c
lynux8888 <algaeee@gmail.com>
2024-04-11 18:55:09 -0700

    updates

--- main.py
+++ main.py
@@ -9,25 +9,25 @@ header = {
 url = "http://10.43.251.42/input_output?model=Supermicro"
 
 
-# smc = Source(url=url, header=header)
-# base_data = smc.request_data()  # assigned the data into the base_data variable
-# headings = ("Rack", "System SN.", "Status", "Time Taken")
+smc = Source(url=url, header=header)
+base_data = smc.request_data()  # assigned the data into the base_data variable
+headings = ("Rack", "System SN.", "Status", "Time Taken")
 
 
 app = Flask(__name__)
 
 
-# @app.route("/", methods=["GET", "POST"])
-# def index():
-#     if request.method == "POST":
-#         serial_number_list = [request.form.get("serial_numbers")]
-#         for i in serial_number_list:
-#             print(i.strip("\n"))
-#     return render_template("index.html", headings=headings, data=base_data[0:10])
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        serial_number_list = [request.form.get("serial_numbers")]
+        for i in serial_number_list:
+            print(i.strip("\n"))
+    return render_template("index.html", headings=headings, data=base_data[0:10])
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
 
 
 if __name__ == "__main__":
