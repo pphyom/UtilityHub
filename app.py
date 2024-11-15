31286007a95fcd023218e5cde6ef52af867ef39b
lynux8888 <algaeee@gmail.com>
2024-11-14 18:16:38 -0800

    loading function added for bios/ipmi version download

--- app.py
+++ app.py
@@ -194,22 +194,25 @@ def cburn_log():
 
 import random
 
-item = random.randint(0, 10)
 
 @app.route("/get_bios_ver", methods=["GET"])
 def get_bios_ver():
-    data = {"test": "test"}
+    item = random.randint(0, 10)
+    data = {"test": item}
     return jsonify(data)
 
 
 @app.route("/get_ipmi_ver", methods=["GET"])
 def get_ipmi_ver():
-    ip_list = session.get("ip_list", [])
-    temp = []
-    for i in ip_list:
-        ver = get_bios_ipmi_ver(i, ipmitool_cmd["ipmi_ver"])
-        temp.append(ver)
-    return jsonify(temp)
+    # ip_list = session.get("ip_list", [])
+    # temp = []
+    # for ip in ip_list:
+    #     ver = get_bios_ipmi_ver(ip, ipmitool_cmd["ipmi_ver"])
+    #     temp.append(ver)
+    # return jsonify(temp)
+    item = random.randint(0, 10)
+    data = {"test": item}
+    return jsonify(data)
 
 
 @app.route("/tools", methods=["GET", "POST"])
@@ -225,6 +228,7 @@ def tools():
             ip_list.append(ip)
             # ipmi_ver = get_bios_ipmi_ver(ip_list, ipmitool_cmd["ipmi_ver"])
         session["ip_list"] = ip_list
+        ic(ip_list)
         # tempIP = get_ip_172(outfile["part_list"], outfile["sub_sn"], good_list)
         #     temp.append(outfile["order_num"])
         return render_template("tools.html", 
